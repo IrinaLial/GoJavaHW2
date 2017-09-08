d9889b6c43c21c885e39240d69666bd21cd42c7c
Iryna Lialenko <lyalenko.iryna@gmail.com>
2017-09-08 17:33:40 +0300

    Add 3 variants

--- src/GoJavaHW2.java
+++ src/GoJavaHW2.java
@@ -16,5 +16,24 @@ public class GoJavaHW2 {
 
         System.out.println("Напишите свое хобби: ");
         String hobby = in.nextLine();
+
+        System.out.println("----------------------------");
+        System.out.println("Имя: " + name);
+        System.out.println("Город: " + city);
+        System.out.println("Возраст: " + age);
+        System.out.println("Хобби: " + hobby);
+
+        System.out.println("----------------------------");
+
+        System.out.println("Человек по имени " + name + " живет в городе " + city + ".");
+        System.out.println("Этому человеку " + age  + " и он любит заниматься " + hobby + ".");
+
+        System.out.println("----------------------------");
+
+        System.out.println(name + " - " + "имя");
+        System.out.println(city + " - " + "город");
+        System.out.println(age + " - " + "возраст");
+        System.out.println(hobby + " - " + "хобби");
+        System.out.println("----------------------------");
     }
 }
